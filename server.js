a978d34d5cfc700a162814bff462743672945b72
itsmeeSanj <itsmeesanj@gmail.com>
2025-03-27 12:52:25 -0400

    add:package

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const app = express();
+
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+// Fallback to default port if not set in .env
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+
+app.listen(PORT, function(){
+    console.log(`Server running on :http://localhost:${PORT}`)
+})
