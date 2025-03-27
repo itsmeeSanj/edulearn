e5e6e3680ccb0f332470fb2bf49bcd5356070368
itsmeeSanj <itsmeesanj@gmail.com>
2025-03-27 12:07:17 -0400

    add:Project Musical

--- js/addToCart.js
+++ js/addToCart.js
@@ -11,6 +11,7 @@ let exists = false;
 addToCartButtons.forEach((button) => {
   button.addEventListener("click", function (e) {
     exists = false;
+<<<<<<< HEAD
     const selectedCourse = e.target.closest(".course_block");
     const id = selectedCourse.dataset.id;
     const courseInfo = selectedCourse.querySelector(".course_content");
@@ -23,6 +24,47 @@ addToCartButtons.forEach((button) => {
     const lectures = courseInfo.querySelector(".lectures").textContent.trim();
     const hours = courseInfo.querySelector(".hours").textContent.trim();
     const price = courseInfo.querySelector(".price").textContent.trim();
+=======
+    let id = null;
+    let imgSource = null;
+    let title = null;
+    let author = null;
+    let hours = null;
+    let lectures = null;
+    let price = null;
+    let selectedCourse = null;
+    let courseInfo = null;
+
+    if (e.target.closest(".course_block") !== null) {
+      selectedCourse = e.target.closest(".course_block");
+      id = selectedCourse.dataset.id;
+      courseInfo = selectedCourse.querySelector(".course_content");
+      imgSource = selectedCourse.querySelector("img").src.trim();
+      title = courseInfo
+          .querySelector("a")
+          .textContent.replace("\n", "")
+          .trim();
+      author = courseInfo.querySelector("p").textContent.trim();
+      lectures = courseInfo.querySelector(".lectures").textContent.trim();
+      hours = courseInfo.querySelector(".hours").textContent.trim();
+      price = courseInfo.querySelector(".price").textContent.trim();
+    } else {
+      selectedCourse = e.target.closest(".course-item");
+      id = selectedCourse.dataset.id;
+      courseInfo = selectedCourse.querySelector(".course-info");
+      imgSource = selectedCourse.querySelector("img").src.trim();
+      title = courseInfo.querySelector("h2").textContent.replace("\n", "").trim();
+      author = courseInfo.querySelector(".author").textContent.trim();
+      const courseDetail = courseInfo.querySelector(".course_detail").textContent.trim();
+      // Find the positions of specific keywords to extract substrings
+      const hoursEndIndex = courseDetail.indexOf(" total hours");
+      const lecturesStartIndex = courseDetail.indexOf("•") + 1;
+      const lecturesEndIndex = courseDetail.indexOf(" lectures");
+      lectures = courseDetail.substring(lecturesStartIndex, lecturesEndIndex).trim();
+      hours = courseDetail.substring(0, hoursEndIndex).trim();
+      price = courseInfo.querySelector(".price").textContent.trim();
+    }
+>>>>>>> 1e9616e (add:project)
 
     const courseDetails = {
       id: id,
@@ -68,6 +110,11 @@ function addToCart(courseDetails) {
   }
   // Save the updated cart back to localStorage
   localStorage.setItem("Cart", JSON.stringify(courseCart));
+<<<<<<< HEAD
+=======
+
+  appendAlertSucess("Course added to cart.", "success");
+>>>>>>> 1e9616e (add:project)
 }
 
 function displayCart() {
@@ -104,13 +151,23 @@ function displayCart() {
                   <span class="price">${course.price}</span>
                 </div>
                 <div>
+<<<<<<< HEAD
                 <button class="mt-1 btn btn-danger btn-sm">Delete</button>
+=======
+                <button class="delete-cart-item mt-1 btn btn-danger btn-sm" data-id="${course.id}">Delete</button>
+>>>>>>> 1e9616e (add:project)
                 </div>
               </div>
             </div>
         `;
 
     cartContainer.innerHTML += courseHTML;
+<<<<<<< HEAD
+=======
+
+    // Add delete listener
+    deleteCartItemEventListeners();
+>>>>>>> 1e9616e (add:project)
   });
 }
 
@@ -136,6 +193,10 @@ function computePrices() {
   $("subTotal").textContent = `$${sum.toFixed(2)}`;
   $("subTotalvalue").textContent = `$${sum.toFixed(2)}`;
   $("totalPayable").textContent = `$${(sum * 0.15).toFixed(2)}`;
+<<<<<<< HEAD
+=======
+  $("subTotalvalue").textContent = `$${(sum+(sum * 0.15)).toFixed(2)}`;
+>>>>>>> 1e9616e (add:project)
 }
 
 // Call displayCart when the page loads
@@ -144,3 +205,32 @@ document.addEventListener("DOMContentLoaded", () => {
   updateNumCart();
   computePrices();
 });
+<<<<<<< HEAD
+=======
+
+function deleteCartItemEventListeners() {
+  const deleteButtons = document.querySelectorAll(".delete-cart-item");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const id = e.target.dataset.id;
+      deleteFromCart(id);
+      displayCart(); // Update cart display after deletion
+      updateNumCart(); // Update cart number
+      computePrices(); // Update prices
+    });
+  });
+}
+
+function deleteFromCart(id) {
+  // Get Cart from local storage
+  const courseCart = JSON.parse(localStorage.getItem("Cart")) || [];
+
+  // Filter out the course to be deleted
+  const updatedCart = courseCart.filter((course) => course.id !== id);
+
+  // Save the updated cart back to localStorage
+  localStorage.setItem("Cart", JSON.stringify(updatedCart));
+}
+
+>>>>>>> 1e9616e (add:project)
